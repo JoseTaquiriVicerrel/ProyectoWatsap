714a59f22b505bd003c28c6a7a213d72017014c1
JoseTaquiriVicerrel <57278949+JoseTaquiriVicerrel@users.noreply.github.com>
2019-11-03 16:11:20 -0500

    Añadi Toolbar

--- app/src/main/java/com/example/proyectowatsap/MainActivity.java
+++ app/src/main/java/com/example/proyectowatsap/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.proyectowatsap;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar mToolbar;
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToolbar=findViewById(R.id.main_page_toolbar);
+        setActionBar(mToolbar);
+        getSupportActionBar().setTitle("Watsap");
     }
 }
